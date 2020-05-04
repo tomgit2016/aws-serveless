85bd1e37890d25705c93716e004543f3e4eecd50
tomgit2016 <22808106+tomgit2016@users.noreply.github.com>
2020-05-03 22:46:50 -0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_jeCQVsZF5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 's96eh5irm2lnhla7md58fr8h1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://xjsdac7i1d.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
